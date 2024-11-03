41e19315977a3535d8e5d611de0a2c75a07ec641
SteamDeck <nprimoy@gmail.com>
2024-11-03 12:49:13 +0100

    prueba desde la steam deck

--- Tema 2/Ej02.py
+++ Tema 2/Ej02.py
@@ -1,7 +1,7 @@
 # Diseñar una función que tenga como entrada tres números enteros y nos devuelva el mayor de los tres.
 #  Incluir el algoritmo principal que realice la llamada a dicha función.
 
-
+#knfvsnbgsnlk
 
 
 
