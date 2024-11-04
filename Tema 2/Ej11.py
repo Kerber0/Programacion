5cb48a4a0dc074f9648977f35526b3aaa496b664
Nico <nprimoy@gmail.com>
2024-11-04 18:25:44 +0100

    Ejercicios repetidos eliminados y uno a dos ejercicios hechos

--- Tema 2/Ej11.py
+++ Tema 2/Ej11.py
@@ -1,2 +1,26 @@
 # Diseñar una función que determine la posición del valor máximo de una lista de enteros y 
 # otra para indicar cuál es ese valor máximo. Incluir un programa que pruebe ambas funciones.
+
+n = [1 , 5 , 6, 8, 6, 4, 7, 8, 8, 9, 6, 4, 5, 3, 1]
+def valor_max(n) ->int:
+  max_val=n[0] 
+  for i in n:
+    if i > max_val:
+      max_val = i
+  return(max_val)
+
+max_val = valor_max(n)
+
+def position_max(n) -> int:
+  for i in n:
+    if i == valor_max:
+      return (i)
+      break
+    else:
+      continue
+
+position = position_max(n)
+print(max_val)
+print(position)
+
+
